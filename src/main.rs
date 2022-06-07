a570e5961a5ba5148d621a57319c214fc0410adf
Ishtmeet Singh <ish.rissam@gmail.com>
2022-06-07 12:49:45 +0530

    add modular functions

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,21 @@ struct Config<'a> {
     query: &'a str,
 }
 
+impl<'a> Config<'a> {
+    fn new(args: &'a [String]) -> Self {
+        let filename: &String = &args[1];
+        let query: &String = &args[2];
+        Self { filename, query }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let Config { filename, query } = parse_args(&args);
+    let Config { filename, query } = Config::new(&args);
 
     let file_contents: String = read_file(&filename, &query).unwrap();
     println!("{}", file_contents);
-}
-
-fn parse_args<'a>(args: &'a [String]) -> Config {
-    let filename: &String = &args[1];
-    let query: &String = &args[2];
-    Config { filename, query }
 }
 
 fn read_file(filename: &str, query: &str) -> Result<String, io::Error> {
