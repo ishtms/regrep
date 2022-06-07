944b0ce662d34db9c3db979f2f474403744e6d63
Ishtmeet Singh <ish.rissam@gmail.com>
2022-06-07 11:49:40 +0530

    add basic argument parsing code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let args_slice: &[String] = &args[1..];
+    println!("{:#?}", args_slice);
+
+    // Alternately we can hard code the address/reference of the arguments into variables
+    let file_name: &String = &args[1];
+    let regex_to_search: &String = &args[2];
+
+    println!("Searching for {} in file: {}", regex_to_search, file_name);
 }
