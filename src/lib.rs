980dcd425fb5b160b19a04b7c170b0db6f8cc5da
Ishtmeet Singh <ish.rissam@gmail.com>
2022-06-07 21:26:53 +0530

    allow use of case sensitive search pattern"

--- src/lib.rs
+++ src/lib.rs
@@ -18,32 +18,46 @@ impl<'a> Config<'a> {
     }
 }
 
-pub fn read_file(filename: &str, query: &str) -> Result<String, io::Error> {
+pub fn read_file(filename: &str) -> Result<String, io::Error> {
     let file_contents: String = fs::read_to_string(filename)?;
     Ok(file_contents)
 }
 
 pub fn run(filename: &str, query: &str) -> Result<(), Box<dyn Error>> {
-    let file_contents: String = read_file(&filename, &query)?;
+    let file_contents: String = read_file(&filename)?;
     println!("\n*------------ OUTPUT ------------*\n");
-    let matches: Vec<(&str, usize)> = search(query, &file_contents);
-    println!("Total {} matches found!", matches.len());
-    for curr_line in matches.iter() {
-        println!("Line {}: {}", curr_line.1, curr_line.0);
-    }
+    let matched_entries: Option<Vec<(&str, usize)>> = search(query, &file_contents);
 
-    println!("\n*---------- OUTPUT END ----------*\n");
-    Ok(())
+    if let Some(matches) = matched_entries {
+        println!("Total {} matches found!", matches.len());
+        for curr_line in matches.iter() {
+            println!("Line {}: {}", curr_line.1, curr_line.0);
+        }
+
+        println!("\n*---------- OUTPUT END ----------*\n");
+        Ok(())
+    } else {
+        println!("No match found.");
+        Ok(())
+    }
 }
 
-fn search<'a>(query: &'a str, contents: &'a str) -> Vec<(&'a str, usize)> {
+fn search<'a>(query: &'a str, contents: &'a str) -> Option<Vec<(&'a str, usize)>> {
     let mut result_vector = Vec::new();
     for (index, curr_line) in contents.lines().enumerate() {
         if curr_line.contains(query) {
             result_vector.push((curr_line, index + 1));
         }
     }
-    result_vector
+    if result_vector.len() > 0 {
+        Some(result_vector)
+    } else {
+        None
+    }
+}
+
+fn search_case_insensitive<'a>(query: &'a str, contents: &'a str) -> Option<Vec<(&'a str, usize)>> {
+    Some(vec![("", 1)])
 }
 
 #[cfg(test)]
@@ -51,13 +65,33 @@ mod test {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let contents = "\
 fn main() {
     doSomething();
 }
 main();";
         let query = "doS";
-        assert_eq!(vec![("    doSomething();", 2)], search(query, contents));
+        let result = search(query, contents);
+        assert_eq!(result, Some(vec![("    doSomething();", 2)]));
+
+        let query_to_fail = "dos";
+        let result = search(query_to_fail, contents);
+        assert_eq!(result, None);
+    }
+    #[test]
+    fn case_insensitive() {
+        let contents = "\
+fn main() {
+    doSomething();
+}";
+        let query = "dos";
+
+        let result = search(query, contents);
+        assert_eq!(result, Some(vec![("    doSomething();", 2)]));
+
+        let query = "dOs";
+        let result = search(query, contents);
+        assert_eq!(result, Some(vec![("    doSomething();", 2)]));
     }
 }
