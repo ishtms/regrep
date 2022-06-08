6ad3e9cfa21cea0338a07a46d40b557a17a47a99
Ishtmeet Singh <ish.rissam@gmail.com>
2022-06-08 10:32:54 +0530

    allow errors to be printed to the std error stream instead of the output stream

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let configuration: Config = Config::new(&args).unwrap_or_else(|err| {
-        println!("There was an error while parsing the arguments: {}", err);
+        eprintln!("There was an error while parsing the arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(error) = regrep::run(configuration) {
-        println!(
+        eprintln!(
             "Something went wrong while parsing the contents of the file: {}",
             error
         );
