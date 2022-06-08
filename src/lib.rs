23760518b6b520cecf23b079f2f2e12997686c31
Ishtmeet Singh <ish.rissam@gmail.com>
2022-06-08 09:32:20 +0530

    update ::run to take a configuration object instead of destructurin

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 use std::io;
 
+pub const CASE_INSENSITIVE: &str = "CASE_INSENSITIVE";
+
 pub struct Config<'a> {
     pub filename: &'a str,
     pub query: &'a str,
+    pub case_sensitive: bool,
 }
 
 impl<'a> Config<'a> {
@@ -14,7 +18,14 @@ impl<'a> Config<'a> {
         }
         let filename: &String = &args[1];
         let query: &String = &args[2];
-        Ok(Self { filename, query })
+
+        let case_sensitive = env::var(CASE_INSENSITIVE).is_err();
+
+        Ok(Self {
+            filename,
+            query,
+            case_sensitive,
+        })
     }
 }
 
@@ -23,18 +34,28 @@ pub fn read_file(filename: &str) -> Result<String, io::Error> {
     Ok(file_contents)
 }
 
-pub fn run(filename: &str, query: &str) -> Result<(), Box<dyn Error>> {
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let Config {
+        query,
+        filename,
+        case_sensitive,
+    } = config;
+
     let file_contents: String = read_file(&filename)?;
-    println!("\n*------------ OUTPUT ------------*\n");
-    let matched_entries: Option<Vec<(&str, usize)>> = search(query, &file_contents);
+    let matched_entries: Option<Vec<(&str, usize)>>;
+
+    if case_sensitive {
+        matched_entries = search(query, &file_contents);
+    } else {
+        matched_entries = search_case_insensitive(query, &file_contents);
+    }
 
     if let Some(matches) = matched_entries {
-        println!("Total {} matches found!", matches.len());
         for curr_line in matches.iter() {
             println!("Line {}: {}", curr_line.1, curr_line.0);
         }
+        println!("\nTotal {} matches found!", matches.len());
 
-        println!("\n*---------- OUTPUT END ----------*\n");
         Ok(())
     } else {
         println!("No match found.");
@@ -57,7 +78,18 @@ fn search<'a>(query: &'a str, contents: &'a str) -> Option<Vec<(&'a str, usize)>
 }
 
 fn search_case_insensitive<'a>(query: &'a str, contents: &'a str) -> Option<Vec<(&'a str, usize)>> {
-    Some(vec![("", 1)])
+    let mut result_vector = Vec::new();
+
+    for (line_number, line_content) in contents.lines().enumerate() {
+        if line_content.to_lowercase().contains(&query.to_lowercase()) {
+            result_vector.push((line_content, line_number + 1));
+        }
+    }
+    if result_vector.len() > 0 {
+        Some(result_vector)
+    } else {
+        None
+    }
 }
 
 #[cfg(test)]
@@ -87,11 +119,11 @@ fn main() {
 }";
         let query = "dos";
 
-        let result = search(query, contents);
+        let result = search_case_insensitive(query, contents);
         assert_eq!(result, Some(vec![("    doSomething();", 2)]));
 
         let query = "dOs";
-        let result = search(query, contents);
+        let result = search_case_insensitive(query, contents);
         assert_eq!(result, Some(vec![("    doSomething();", 2)]));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ use regrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let Config { filename, query } = Config::new(&args).unwrap_or_else(|err| {
+    let configuration: Config = Config::new(&args).unwrap_or_else(|err| {
         println!("There was an error while parsing the arguments: {}", err);
         process::exit(1);
     });
 
-    if let Err(error) = regrep::run(filename, query) {
+    if let Err(error) = regrep::run(configuration) {
         println!(
             "Something went wrong while parsing the contents of the file: {}",
             error
